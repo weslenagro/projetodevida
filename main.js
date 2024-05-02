0b2d73280f1f842eb27a961c27c85802bf26ff13
weslenagro <128072137+weslenagro@users.noreply.github.com>
2024-05-02 14:44:44 -0300

    -wcdfgtgr

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-09-28T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-31T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-31T00:00:00");
-const tempoObjetivo4 = new Date("2024-09-28T00:00:00");
+const tempoObjetivo1 = new Date("2026-02-28T00:00:00");
+const tempoObjetivo2 = new Date("2029-02-31T00:00:00");
+const tempoObjetivo3 = new Date("2027-02-31T00:00:00");
+const tempoObjetivo4 = new Date("2025-02-28T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
